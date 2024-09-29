c097d9979a914c48539e1c3db7990ab865168a1e
Lachlan Coote <63722113+LachlanCoote@users.noreply.github.com>
2024-09-29 16:01:52 +1000

    baseline packages + nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+  i18n: {
+    vueI18n: './i18n.config.ts',
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxtjs/i18n',
+    '@pinia/nuxt',
+  ],
+  typescript: {
+    typeCheck: true,
+  },
 })
