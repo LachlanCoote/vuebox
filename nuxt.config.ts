21a5423587035d0668a7c04a499bee8c80a20c99
Lachlan Coote <lachlan.coote@swiftpos.com.au>
2024-10-19 13:55:52 +1000

    Tidy up nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,13 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', href: '/favicon.ico' },
       ],
+      meta: [
+        { name: 'description', content: 'A sandbox starter app for Nuxt 3'},
+        { name: 'theme-color', content: '#313F48' },
+      ],
+      title: 'Vuebox',
     },
   },
   compatibilityDate: '2024-04-03',
@@ -14,8 +19,16 @@ export default defineNuxtConfig({
       standalone: false,
     },
   },
+  experimental: {
+    payloadExtraction: false,
+  },
   i18n: {
     vueI18n: './i18n.config.ts',
+  },
+  icon: {
+    clientBundle: {
+      scan: true, // future enabled by default
+    },
   },
   future: {
     compatibilityVersion: 4,
@@ -27,6 +40,31 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
     '@vite-pwa/nuxt',
   ],
+  pwa: {
+    includeAssets: ['favicon.ico'],
+    manifest: {
+      description: 'A sandbox starter app for Nuxt 3',
+      name: 'Vuebox',
+      short_name: 'VB',
+      theme_color: '#313F48',
+      icons: [{
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },{
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      }],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+  },
+  routeRules: {
+    '/': { prerender: true },
+  },
+  ssr: false,
   typescript: {
     typeCheck: true,
   },
